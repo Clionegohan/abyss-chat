988e1743ffac8a7bf518ce1de165f0a0760d7350
Clionegohan <sawakiguchik@gmail.com>
2025-05-15 20:16:02 +0900

    chore(frontend): UI要素の微調整（Headerなど）

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,10 +5,10 @@ export const metadata = {
 
 export default function Home() {
   return (
-      <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <div className="min-h-screen bg-blue-500 flex items-center justify-center px-4">
           <div className="text-center space-y-4">
-              <h1 className="text-4xl font-bold">ようこそ、Abyss Chatへ！</h1>
-              <p className="text-lg text-gray-600">
+              <h1 className="text-4xl font-bold text-white drop-shadow">ようこそ、Abyss Chatへ！</h1>
+              <p className="text-lg text-white/90">
                 深海のように静かで安心できる場所で、やすらぎのひとときを。
               </p>
           </div>
