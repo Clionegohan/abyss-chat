3b4ba321e3db99ea93c9afe8817915629083ac01
Clionegohan <sawakiguchik@gmail.com>
2025-05-15 20:12:05 +0900

    refactor(user): userModelをservice層に分離

---
+++ backend/src/services/userServices.ts
@@ -0,0 +1,16 @@
+import { getUserByIdModel, createUserModel, userExistsByEmail } from '../models/userModel';
+import { hashPassword } from '../utils/hash';
+import { CreateUserInput } from '../types/user';
+
+export const getUserByIdService = async (id: string) => {
+    return await getUserByIdModel(id);
+};
+
+export const createUserService = async (input: CreateUserInput) => {
+    const exists = await userExistsByEmail(input.email);
+    if (exists) throw new Error('そのメールアドレスは既に使われています。');
+
+    const passwordHash = await hashPassword(input.password);
+    return await createUserModel({ ...input, passwordHash });
+};
+
